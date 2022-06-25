810f3124f20f5adccd1f1ac44edd6a98dfdd2d90
Tarun <tarunpsv@gmail.com>
2022-06-25 11:27:38 +0530

    Second Commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const connectDB = require('./server/database/connection');
 const app = express();
 
 dotenv.config({path : 'config.env'})
-const PORT = process.env.PORT || 3000
+const port = process.env.PORT || 3000
 
 //log requests
 app.use(morgan('tiny'));
@@ -32,4 +32,4 @@ app.use('/js',express.static(path.resolve(__dirname,"assets/js")))
 //load routers
 app.use('/',require('./server/routes/router'))
 
-app.listen(PORT,()=> {console.log(`Server is running on http://localhost:${PORT}`)});
+app.listen(port,()=> {console.log(`Server is running on http://localhost:${PORT}`)});
